abd708609e8e160244bce08d6e868cad6fd54238
nhnaahid <nhnahid.iu@gmail.com>
2024-06-06 04:54:54 +0600

    users api added

--- index.js
+++ index.js
@@ -8,7 +8,18 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 // middleware
-app.use(cors());
+app.use(
+    cors({
+        origin: [
+            "http://localhost:5173",
+            "http://localhost:5174",
+            "http://localhost:5175",
+            "https://skillup-57533.web.app",
+            "https://skillup-57533.firebaseapp.com",
+        ],
+        credentials: true,
+    })
+);
 app.use(express.json());
 
 
@@ -30,7 +41,7 @@ async function run() {
         // Connect the client to the server	(optional starting in v4.7)
         // await client.connect();
 
-
+        const userCollection = client.db("skillupDB").collection("users");
 
         // jwt related api
         app.post('/jwt', async (req, res) => {
@@ -38,6 +49,22 @@ async function run() {
             const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
             res.send({ token });
         })
+
+        // user related api
+        app.get('/users', async (req, res) => {
+            const result = await userCollection.find().toArray();
+            res.send(result);
+        });
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const query = { email: user.email }
+            const isUserExist = await userCollection.findOne(query);
+            if (isUserExist) {
+                return res.send({ message: 'Existing User', insertedId: null })
+            }
+            const result = await userCollection.insertOne(user);
+            res.send(result);
+        });
 
 
         // Send a ping to confirm a successful connection
